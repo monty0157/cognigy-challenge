db7fc0ec74d44e453b5e29271d2533398daef2c1
Kasper Moerch Zederkof <kaspermz@hotmail.com>
2017-06-15 21:24:16 +0200

    Created wrapper and main card component for chat

---
+++ dashboard/src/components/Wrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Wrapper = function Wrapper({children}) {
+
+  return(
+    <div className="flex justify-center items-center h-100">
+      {children}
+    </div>
+  )
+}
+
+export default Wrapper;
---
+++ dashboard/src/layouts/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Wrapper from '../components/Wrapper';
+import ChatCard from '../components/ChatCard';
+
+const MainLayout = function MainLayout() {
+
+  return (
+    <Wrapper>
+      <ChatCard />
+    </Wrapper>
+  )
+}
+
+export default MainLayout;
